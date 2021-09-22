79b2b54b5f89f7059d67bcacd12a2a657cd9901d
omersahin3 <omersahine@hotmail.com>
2021-09-22 15:29:47 +0300

    beta update 0.1

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,21 @@
 let weather = {
-    "apiKey":"e94def9aab582f0735016d5110df5c0f" 
-}
+    apiKey: "49cc8c821cd2aff9af04c9f98c36eb74",
+    fetchWeather: function (city) {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q=" +
+            city +
+            "&units=metric&appid=" +
+            this.apiKey
+        )
+            .then((response) => response.json())
+            .then((data) => this.displayWeather(data));
+            // .then((data) => console.log(data));
+    },
+    displayWeather: function(data) {
+        const{ name } = data;
+        const{ icon, description } = data.weather[0];
+        const{ temp, humidity } = data.main;
+        const { speed } = data.wind;
+        console.log(name,icon,description,temp,humidity,speed);
+    }
+};
