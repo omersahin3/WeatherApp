e8d2f74a0823b73a3b418e3d291cc858a860b0f9
omersahin3 <omersahine@hotmail.com>
2021-09-22 16:49:04 +0300

    beta update 0.2

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -17,5 +17,18 @@ let weather = {
         const{ temp, humidity } = data.main;
         const { speed } = data.wind;
         console.log(name,icon,description,temp,humidity,speed);
-    }
+        document.getElementById('country').textContent = name;
+        document.getElementById('icon').src = "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.getElementById('temp').textContent = temp;
+        document.getElementById('humidity').textContent = humidity + "%";
+        document.getElementById('wind').innerText = speed + " km/h";
+        document.body.style.backgroundImage =
+        "url('https://source.unsplash.com/1920x1080/?" + name + "')";
+    },
+    search: function () {
+        this.fetchWeather(document.querySelector('.find').value);
+    },
 };
+document.querySelector('#search').addEventListener("click", function () {
+    weather.search();
+});
