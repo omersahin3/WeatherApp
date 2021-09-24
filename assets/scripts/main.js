f4b4de13cb8774023a9b9edeaebfd6e5131d60a9
omersahin3 <omersahine@hotmail.com>
2021-09-24 10:09:11 +0300

    beta update0.4

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -12,19 +12,24 @@ let weather = {
             .then((response) => response.json())
             .then((data) => {
                 if (type == "weather")
+                {
                 this.displayWeather (data)
+                console.log(data)
+                }
                 else if(type == "forecast")
                 this.displayForecast (data)
-                // console.log(data);
             });
     },
     displayWeather: function(data) {
-        const{ name } = data;
+        const{ name , timezone } = data;
         const{ icon, description } = data.weather[0];
         const{ temp, humidity, pressure } = data.main;
         const { speed } = data.wind;
+        const { sunrise, sunset} = data.sys;
+        const { lat, lon} = data.coord;
         console.log(name,icon,description,temp,humidity,speed);
         document.getElementById('country').textContent = name;
+        document.getElementById('location').textContent = (String(lat).slice(0,6)) + 'N ' + (String(lon).slice(0,6)) +'E';
         document.getElementById('icon').src = "https://openweathermap.org/img/wn/" + icon + ".png";
         document.getElementById('description').textContent = description;
         document.getElementById('temp').textContent = temp;
@@ -33,6 +38,40 @@ let weather = {
         document.getElementById('wind').innerText = speed + " km/h";
         document.body.style.backgroundImage =
         "url('https://source.unsplash.com/1920x1080/?" + name + "')";
+
+        var zone = (timezone - 10800)/3600;
+        console.log(zone);
+
+        var utcSeconds = sunrise;
+        var r = new Date(0); 
+        r.setUTCSeconds(utcSeconds);
+        const hour = r.getHours();
+        const minutes = r.getMinutes();
+        const ampm = hour >=12 ? 'pm' : 'am';
+        document.getElementById('sunrise').textContent = (hour + zone) + ":" + minutes + " " + ampm;
+
+        var utcSeconds2 = sunset;
+        var s = new Date(0); 
+        s.setUTCSeconds(utcSeconds2);
+        const hour2 = s.getHours();
+        const minutes2 = s.getMinutes();
+        const ampm2 = hour2 >=12 ? 'pm' : 'am';
+        document.getElementById('sunset').textContent = (hour2 + zone) + ":" + minutes2 + " " + ampm2;
+
+        var date = new Date();
+        const hour3 = date.getHours();
+        const minutes3 = date.getMinutes(); 
+        const ampm3 = hour3 >=12 ? 'PM' : 'AM';
+        // document.querySelector('#time').textContent = hour3 + ":" + minutes3 + " " + ampm3;
+
+        document.querySelector('#time').innerHTML =`
+        ${(hour3 + zone) + ":" + minutes3}
+        <span id="am-pm">${ampm3}</span>
+        `
+
+        var options = { weekday: 'long', day: 'numeric' , month: 'short',};
+        document.querySelector('#date').textContent = r.toLocaleString("en-US", options);
+        
     },
     displayForecast: function(data) {
         const{ name } = data.city;
@@ -93,3 +132,4 @@ document.querySelector('.find').addEventListener("keyup", function(event) {
         weather.search();
     }
 })
+
