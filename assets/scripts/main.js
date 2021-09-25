db446fd49ad2b5a2c17124789f295b3a7e11931d
omersahin3 <omersahine@hotmail.com>
2021-09-25 16:25:29 +0300

    big update

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -13,10 +13,19 @@ let weather = {
                 if (type == "weather")
                 {
                 this.displayWeather (data)
-                console.log(data)
+                // console.log(data)
                 }
                 else if(type == "forecast")
                 this.displayForecast (data)
+            });
+    },
+    fetchCountry: function (query) {
+        fetch(
+            "https://raw.githubusercontent.com/dr5hn/countries-states-cities-database/master/countries%2Bstates%2Bcities.json"
+        )
+            .then((response) => response.json())
+            .then((data) => {
+                this.displayCountry (data,query)
             });
     },
     displayWeather: function(data) {
@@ -26,7 +35,7 @@ let weather = {
         const { speed } = data.wind;
         const { sunrise, sunset} = data.sys;
         const { lat, lon} = data.coord;
-        console.log(name,icon,description,temp,humidity,speed);
+        // console.log(name,icon,description,temp,humidity,speed);
         document.getElementById('country').textContent = name;
         document.getElementById('location').textContent = (String(lat).slice(0,6)) + 'N ' + (String(lon).slice(0,6)) +'E';
         document.getElementById('icon').src = "https://openweathermap.org/img/wn/" + icon + ".png";
@@ -42,7 +51,6 @@ let weather = {
             document.body.style.backgroundImage = "url('https://images.unsplash.com/photo-1565036045177-025ab34fb5b3?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=1080&ixid=MnwxfDB8MXxyYW5kb218MHx8SXN0YW5idWx8fHx8fHwxNjMyNDY4MzMx&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1920')";
         }
         var zone = (timezone - 10800)/3600;
-        console.log(zone);
 
         var utcSeconds = sunrise;
         var r = new Date(0); 
@@ -76,9 +84,7 @@ let weather = {
     displayForecast: function(data) {
         const{ icon } = data.list[0].weather[0];
         const{ temp } = data.list[0].main;
-        console.log(name,icon,temp);
-        // document.getElementById('icon').src = "https://openweathermap.org/img/wn/" + icon + ".png";
-        // document.getElementById('temp').textContent = temp;
+        console.log(icon,temp);
         let otherDayForcast = ''
         for(i=0; i<40; i+=8)
         {
@@ -88,7 +94,6 @@ let weather = {
 
             let d = String(new Date(dt_txt));
             d=d.slice(0,3);
-            console.log(d);
             
             if(i==0)
             {
@@ -114,21 +119,50 @@ let weather = {
             document.querySelector('.main').style.visibility = "visible"; // ilk başta hidden olan şeyi visible 
         }
     },
-    search: function () {
-        this.fetchWeather(document.querySelector('.find').value,
+    displayCountry: function(data,query){
+        const querylist = [];
+        const q = String(query).toLowerCase();
+
+        console.log(data[227]['states']);
+        const states = data[227]['states'];
+        let querylistHtml = '';
+
+        for(let i = 0; i< states.length ; i++){
+            const cities = states[i]['cities'];
+            for(let j = 0; j< cities.length ; j++){
+                const name = String(cities[j]['name']).toLowerCase();
+                if(name.includes(q)) {
+                    querylist.push(name);
+                    querylistHtml += `<li onmousedown="weather.search('${name}')" class="queryLi" value="${name}">${name}</li>`;
+                    document.getElementById('rlist').innerHTML = querylistHtml;
+                }
+                if(querylist.length == 5)
+                {
+                    i = states.length
+                    j = cities.length
+                }
+            }
+        }
+    },
+    search: function (value) {
+        this.fetchWeather(value,
         "forecast");
-        this.fetchWeather(document.querySelector('.find').value,
+        this.fetchWeather(value,
         "weather");
         first = false;
     },
 };
 document.querySelector('#search').addEventListener("click", function () {
-    weather.search();
+    weather.search(document.querySelector('.find').value);
 });
 document.querySelector('.find').addEventListener("keyup", function(event) {
     if (event.key == "Enter") {
-        weather.search();
+        weather.search(document.querySelector('.find').value);
     }
-})
+});
+document.querySelector('.find').addEventListener("input", function () {
+    console.log(document.querySelector('.find').value);
+    weather.fetchCountry(document.querySelector('.find').value);
+});
 weather.fetchWeather("istanbul","forecast");
 weather.fetchWeather("istanbul","weather");
